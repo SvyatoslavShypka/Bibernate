244bdd4303132a0ab107bc373fcd0863cfa41167
Svyatoslav Shypka <svyat200909@gmail.com>
2024-07-07 15:24:17 +0200

    start point

--- src/main/resources/db/migration/V2024.07.07.001__create-schema.sql
+++ src/main/resources/db/migration/V2024.07.07.001__create-schema.sql
@@ -1,26 +1,12 @@
-CREATE SCHEMA IF NOT EXISTS access;
 
-CREATE TABLE IF NOT EXISTS access.gender(
-                              gender_id    numeric(1)    primary key,
-                              name_ukr     varchar(50)   not null,
-                              name_eng     varchar(50)   not null
+DROP TABLE IF EXISTS participants;
+CREATE TABLE IF NOT EXISTS participants(
+                             id         SERIAL PRIMARY KEY,
+                             first_name        varchar(50)   not null,
+                             last_name        varchar(50)   not null,
+                             city        varchar(50)   not null,
+                             company        varchar(50)   not null,
+                             position        varchar(50)   not null,
+                             years_of_experience integer,
+                             created_at    timestamp DEFAULT NOW()
 );
-
-CREATE UNIQUE INDEX IF NOT EXISTS ix_gender_gender_id on access.gender (gender_id);
-
-CREATE TABLE IF NOT EXISTS access.users(
-                             user_id         UUID           primary key,
-                             username        varchar(50)   not null,
-                             password        varchar(500)   not null,
-                             role            varchar(32)    not NULL,
-                             enabled         boolean        not null,
-                             nickname        varchar(100)    not null,
-                             birthday        date,
-                             gender_id       integer        not null default 0,
-                             created_date    timestamp,
-                             updated_date    timestamp,
-                             constraint fk_users_gender foreign key(gender_id) references access.gender(gender_id)
-);
-CREATE UNIQUE INDEX IF NOT EXISTS ix_users_user_id   on access.users (user_id);
-CREATE UNIQUE INDEX IF NOT EXISTS ix_users_username  on access.users (username);
-CREATE INDEX IF NOT EXISTS ix_users_gender_id on access.users (gender_id);
---
+++ src/main/resources/db/migration/V2024.07.07.002__populate.sql
@@ -0,0 +1,5 @@
+INSERT INTO participants(first_name, last_name, city, company, position, years_of_experience)
+VALUES  ('Swiatoslaw', 'Szypka', 'Bialystok', 'Private company', 'Junior Java developer', 1),
+        ('Petro', 'Petrenko', 'Lviv', 'Private company1', 'Middle Java developer', 2),
+        ('Sydirw', 'Sydorenko', 'Kyiv', 'Private company2', 'Senior Java developer', 3),
+        ('Ivan', 'Ivanenko', 'Kharkiv', 'Private company3', 'Trainee Java developer', 0);
