23cba7f36b11054e970bb5e395e02f2e9d9790c2
Svyatoslav Shypka <svyat200909@gmail.com>
2024-08-02 14:05:36 +0200

    Stage1.1.6

--- src/main/java/dar3/eu/demo/Main.java
+++ src/main/java/dar3/eu/demo/Main.java
@@ -6,20 +6,6 @@ import dar3.eu.demo.entity.Participant;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-        }
-
-//        System.out.println(fizzBuzz(100));
-
 
         String password = System.getenv("DB_PASSWORD");
         String username = System.getenv("DB_USER");
@@ -29,21 +15,4 @@ public class Main {
         System.out.println(participant);
 
     }
-    public static String fizzBuzz(int n) {
-        String s = "";
-        if (n == 0) {
-            return s;
-        }
-        if ((n % 5) == 0) {
-            s = "Buzz" + s;
-        }
-        if ((n % 3) == 0) {
-            s = "Fizz" + s;
-        }
-        if (s.equals("")) {
-            s = n + "";
-        }
-        return fizzBuzz(n - 1) + s + "\n";
-    }
-
 }
