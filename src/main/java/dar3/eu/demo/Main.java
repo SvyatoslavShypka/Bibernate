ffa2a25c914bdc0ea9c3ccb22b45af34aac984e0
Svyatoslav Shypka <svyat200909@gmail.com>
2024-08-14 14:43:26 +0200

    Stage1.1.6

--- src/main/java/dar3/eu/demo/Main.java
+++ src/main/java/dar3/eu/demo/Main.java
@@ -8,11 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
         String password = System.getenv("DB_PASSWORD");
+
         String username = System.getenv("DB_USER");
         var sessionFactory = new SessionFactoryImpl("jdbc:postgresql://localhost:5433/bibernate", username, password);
 
         var session = sessionFactory.openSession();
         var participant = session.findById(Participant.class, 4);
+        session.close();
         System.out.println(participant);
     }
 }
